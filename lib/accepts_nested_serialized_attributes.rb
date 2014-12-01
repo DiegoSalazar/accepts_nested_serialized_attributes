32f116433eaf68e973d87369314f79fc9f7892f1
Diego Salazar <diego@greyrobot.com>
2014-12-01 17:12:25 -0500

    wrote Gem code, gemspec, README

---
+++ lib/accepts_nested_serialized_attributes.rb
@@ -0,0 +1,17 @@
+require "accepts_nested_serialized_attributes/version"
+
+module AcceptsNestedSerializedAttributes
+  module ActiveModel::Serialization
+    alias_method :original_serializable_hash, :serializable_hash
+
+    def serializable_hash(options = nil)
+      original_serializable_hash(options).tap do |hash|
+        self.class.nested_attributes_options.keys.each do |nested_attributes_key|
+          if (records = hash.delete nested_attributes_key)
+            hash["#{nested_attributes_key}_attributes"] = records
+          end
+        end
+      end
+    end
+  end
+end
